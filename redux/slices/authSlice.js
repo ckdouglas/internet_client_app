c0906d11c0f685f5b5ff8aca59faff87503f1298
ckdouglas <douglas.ck.work@gmail.com>
2024-08-11 20:31:54 +0300

    Update structure

--- redux/slices/authSlice.js
+++ redux/slices/authSlice.js
@@ -4,13 +4,16 @@ const authSlice = createSlice({
     name: 'auth',
     initialState: {
         isAuthenticated: false,
+        user: null,
     },
     reducers: {
-        login(state) {
+        login(state, action) {
             state.isAuthenticated = true;
+            state.user = action.payload;
         },
         logout(state) {
             state.isAuthenticated = false;
+            state.user = null;
         },
     },
 });
